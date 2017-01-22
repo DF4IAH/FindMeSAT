807ac071d724444c0985b5c96ba8600d38187f5a
Ulrich Habel <espero7757@gmx.net>
2017-01-22 20:24:31 +0100

    SW - creating AtmelStudio project FindMeSAT_SW

---
+++ FindMeSAT_SW/FindMeSAT_SW/main.c
@@ -0,0 +1,18 @@
+/*
+ * FindMeSAT
+ * main.c
+ *
+ * Created: 22.01.2017 13:13:47
+ * Author : espero
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
