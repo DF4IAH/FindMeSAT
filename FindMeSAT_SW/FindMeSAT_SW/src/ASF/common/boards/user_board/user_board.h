82129c7f4d06e5b003e92a1b2a0dc48e323c6a63
Ulrich Habel <espero7757@gmx.net>
2017-01-27 00:42:15 +0100

    SW - board information data extended

--- FindMeSAT_SW/FindMeSAT_SW/src/ASF/common/boards/user_board/user_board.h
+++ FindMeSAT_SW/FindMeSAT_SW/src/ASF/common/boards/user_board/user_board.h
@@ -111,19 +111,18 @@
  * I2C bus lines of bus #1 and #2.
  */
 //@{
+#define I2C1_TWI                        &TWIE
+#define I2C1_USART_SCL                  &USARTE0
 #define SDA1_GPIO                       IOPORT_CREATE_PIN(PORTE, 0)
 #define SCL1_GPIO                       IOPORT_CREATE_PIN(PORTE, 1)
+
+#define I2C2_TWI                        &TWIC
+#define I2C2_USART_SCL                  &USARTC0
 #define SDA2_GPIO                       IOPORT_CREATE_PIN(PORTC, 0)
 #define SCL2_GPIO                       IOPORT_CREATE_PIN(PORTC, 1)
 #define I2C2_INT_GPIO                   IOPORT_CREATE_PIN(PORTB, 0)
 
-#if 0
-#define SDA1                            SDA1_GPIO
-#define SCL1                            SCL1_GPIO
-#define SDA2                            SDA2_GPIO
-#define SCL2                            SCL2_GPIO
 #define I2C2_INT                        I2C2_INT_GPIO
-#endif
 //@}
 
 
@@ -133,17 +132,45 @@
  * SPI bus lines of the AX5243 device.
  */
 //@{
-#define AX_IRQ_GPIO                     IOPORT_CREATE_PIN(PORTC, 3)
-#define AX_SEL_GPIO                     IOPORT_CREATE_PIN(PORTC, 4)
-#define AX_MOSI_GPIO                    IOPORT_CREATE_PIN(PORTC, 5)
-#define AX_MISO_GPIO                    IOPORT_CREATE_PIN(PORTC, 6)
-#define AX_CLK_GPIO                     IOPORT_CREATE_PIN(PORTC, 7)
+#define AX_SPI                          &SPIC
+#define AX_SPI_CLK                      &USARTC1
+#define AX_IRQ_PIN                      IOPORT_CREATE_PIN(PORTC, 3)
+#define AX_SEL_PIN                      IOPORT_CREATE_PIN(PORTC, 4)
+#define AX_MOSI_PIN                     IOPORT_CREATE_PIN(PORTC, 5)
+#define AX_MISO_PIN                     IOPORT_CREATE_PIN(PORTC, 6)
+#define AX_CLK_PIN                      IOPORT_CREATE_PIN(PORTC, 7)
 
-#define AX_IRQ                          AX_IRQ_GPIO
-#define AX_SEL                          AX_SEL_GPIO
-#define AX_MOSI                         AX_MOSI_GPIO
-#define AX_MISO                         AX_MISO_GPIO
-#define AX_CLK                          AX_CLK_GPIO
+#define AX_IRQ                          AX_IRQ_PIN
+#define AX_SEL                          AX_SEL_PIN
+#define AX_MOSI                         AX_MOSI_PIN
+#define AX_MISO                         AX_MISO_PIN
+#define AX_CLK                          AX_CLK_PIN
+
+#define AX_INTC_INIT()                  ioport_configure_pin(AX_IRQ_PIN, IOPORT_DIR_INPUT); \
+										PORTC.PIN3CTRL = PORT_ISC0_bm; \
+										PORTC.INT0MASK = PIN3_bm; \
+										PORTC.INTFLAGS = PORT_INT0IF_bm;
+
+#define AX_ISR()                        ISR(PORTC_INT0_vect)
+
+/** Enables the transceiver main interrupt. */
+#define ENABLE_TRX_IRQ()                (PORTC.INTCTRL |= PORT_INT0LVL_gm)
+
+/** Disables the transceiver main interrupt. */
+#define DISABLE_TRX_IRQ()               (PORTC.INTCTRL &= ~PORT_INT0LVL_gm)
+
+/** Clears the transceiver main interrupt. */
+#define CLEAR_TRX_IRQ()                 (PORTC.INTFLAGS = PORT_INT0IF_bm)
+
+/*
+ * This macro saves the trx interrupt status and disables the trx interrupt.
+ */
+#define ENTER_TRX_REGION()   { uint8_t irq_mask = PORTC.INTCTRL; PORTC.INTCTRL &= ~PORT_INT0LVL_gm
+
+/*
+ *  This macro restores the transceiver interrupt status
+ */
+#define LEAVE_TRX_REGION()   PORTC.INTCTRL = irq_mask; }
 //@}
 
 
@@ -174,19 +201,19 @@
  */
 //@{
 #define AD_3V0_ADC_MODULE               ADCA
-#define AD_3V0_ADC_INPUT                ADCCH_POS_PIN0
+//#define AD_3V0_ADC_INPUT              ADCCH_POS_PIN0
 #define AD_3V0_SIGNAL_PIN               IOPORT_CREATE_PIN(PORTA, 0)
 
 #define AD_VCTCXO_ADC_MODULE            ADCA
-#define AD_VCTCXO_ADC_INPUT             ADCCH_POS_PIN1
+//#define AD_VCTCXO_ADC_INPUT           ADCCH_POS_PIN1
 #define AD_VCTCXO_SIGNAL_PIN            IOPORT_CREATE_PIN(PORTA, 1)
 
 #define AD_5V0_ADC_MODULE               ADCA
-#define AD_5V0_ADC_INPUT                ADCCH_POS_PIN2
+//#define AD_5V0_ADC_INPUT              ADCCH_POS_PIN2
 #define AD_5V0_SIGNAL_PIN               IOPORT_CREATE_PIN(PORTA, 2)
 
 #define AD_VBAT_ADC_MODULE              ADCA
-#define AD_VBAT_ADC_INPUT               ADCCH_POS_PIN3
+//#define AD_VBAT_ADC_INPUT             ADCCH_POS_PIN3
 #define AD_VBAT_SIGNAL_PIN              IOPORT_CREATE_PIN(PORTA, 3)
 //@}
 
@@ -198,22 +225,28 @@
  */
 //@{
 #define ADC4_ADC_MODULE                 ADCA
-#define ADC4_ADC_INPUT                  ADCCH_POS_PIN4
+//#define ADC4_ADC_INPUT                ADCCH_POS_PIN4
 #define ADC4_SIGNAL_PIN                 IOPORT_CREATE_PIN(PORTA, 4)
 
 #define ADC5_ADC_MODULE                 ADCA
-#define ADC5_ADC_INPUT                  ADCCH_POS_PIN5
+//#define ADC5_ADC_INPUT                ADCCH_POS_PIN5
 #define ADC5_SIGNAL_PIN                 IOPORT_CREATE_PIN(PORTA, 5)
 
 #define DAC0_ADC_MODULE                 ADCB
-#define DAC0_ADC_INPUT                  ADCCH_POS_PIN2
+//#define DAC0_ADC_INPUT                ADCCH_POS_PIN2
 #define DAC0_SIGNAL_PIN                 IOPORT_CREATE_PIN(PORTB, 2)
 
 #define DAC1_ADC_MODULE                 ADCB
-#define DAC1_ADC_INPUT                  ADCCH_POS_PIN3
+//#define DAC1_ADC_INPUT                ADCCH_POS_PIN3
 #define DAC1_SIGNAL_PIN                 IOPORT_CREATE_PIN(PORTB, 3)
 
 #define OC1A_GPIO                       IOPORT_CREATE_PIN(PORTD, 4)
+
+#define JP181_PIN2                      OC1A_GPIO
+#define JP181_PIN3                      ADC4_SIGNAL_PIN
+#define JP181_PIN4                      DAC0_SIGNAL_PIN
+#define JP181_PIN5                      ADC5_SIGNAL_PIN
+#define JP181_PIN6                      DAC1_SIGNAL_PIN
 
 #if 0
 #define OC1A                            OC1A_GPIO
