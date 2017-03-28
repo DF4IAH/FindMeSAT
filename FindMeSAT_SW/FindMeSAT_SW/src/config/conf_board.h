520c030c4b0897e3a6f4ac71c56aac2fbcc4b175
Ulrich Habel <espero7757@gmx.net>
2017-03-28 23:26:38 +0200

    master - merging all branches together

---
+++ FindMeSAT_SW/FindMeSAT_SW/src/config/conf_board.h
@@ -0,0 +1,20 @@
+/**
+ * \file
+ *
+ * \brief User board configuration for FindMeSAT
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+
+#ifndef CONF_BOARD_H
+#define CONF_BOARD_H
+
+/* USB pins are used */
+//#define CONF_BOARD_USB_PORT
+
+/* Enable USB VBUS detect */
+//#define CONF_BOARD_USB_VBUS_DETECT
+
+#endif // CONF_BOARD_H
