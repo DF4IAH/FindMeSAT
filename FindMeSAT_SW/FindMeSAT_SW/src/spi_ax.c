014ccec3fcb6689e035a3c17ff1d28a14573158e
Ulrich Habel <espero7757@gmx.net>
2017-12-28 01:09:28 +0100

    SW - SPI_AX - 15 MHz even with limited slew rate OK

--- FindMeSAT_SW/FindMeSAT_SW/src/spi_ax.c
+++ FindMeSAT_SW/FindMeSAT_SW/src/spi_ax.c
@@ -32,9 +32,9 @@
 
 void spi_init(void) {
 	/* Set init level */
-	ioport_set_pin_mode(AX_SEL,			IOPORT_INIT_HIGH);										// SEL  inactive
-	ioport_set_pin_mode(AX_MOSI,		IOPORT_INIT_HIGH);										// MOSI inactive
-	ioport_set_pin_mode(AX_CLK,			IOPORT_INIT_HIGH);										// CLK  inactive
+	ioport_set_pin_mode(AX_SEL,			IOPORT_INIT_HIGH | IOPORT_SRL_ENABLED);					// SEL  inactive
+	ioport_set_pin_mode(AX_MOSI,		IOPORT_INIT_HIGH | IOPORT_SRL_ENABLED);					// MOSI inactive
+	ioport_set_pin_mode(AX_CLK,			IOPORT_INIT_HIGH | IOPORT_SRL_ENABLED);					// CLK  inactive
 
 	/* Set input pulling resistors */
 	ioport_set_pin_mode(AX_IRQ,			IOPORT_MODE_PULLUP);
@@ -52,11 +52,13 @@ void spi_start(void) {
 	g_ax_spi_device_conf.id = AX_SEL;
 
 	spi_master_init(&SPI_AX);
-	spi_master_setup_device(&SPI_AX, &g_ax_spi_device_conf, SPI_MODE_0, 10000, 0);				// 10 kHz
+	spi_master_setup_device(&SPI_AX, &g_ax_spi_device_conf, SPI_MODE_0, 15000000, 0);			// ATxmega max. 15 MHz
 	spi_enable(&SPI_AX);
 
 
-	/* TEST CODE */
+	/* TEST CODES */
+
+#ifdef AX_TEST01
 	spi_select_device(&SPI_AX, &g_ax_spi_device_conf);
 	g_ax_spi_packet_buffer[0]	= 0x00;
 	g_ax_spi_packet_buffer[1]	= 0x00;
@@ -66,7 +68,7 @@ void spi_start(void) {
 
 	spi_select_device(&SPI_AX, &g_ax_spi_device_conf);
 	g_ax_spi_packet_buffer[0]	= 0x80 | 0x01;
-	g_ax_spi_packet_buffer[1]	= 0x96;
+	g_ax_spi_packet_buffer[1]	= 0x55;
 	spi_write_packet(&SPI_AX, g_ax_spi_packet_buffer, 2);										// Address 0x01: scratch register R/W - set
 	spi_deselect_device(&SPI_AX, &g_ax_spi_device_conf);
 
@@ -76,7 +78,34 @@ void spi_start(void) {
 	spi_write_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);										// Address 0x01: scratch register R/W - check
 	spi_read_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
 	spi_deselect_device(&SPI_AX, &g_ax_spi_device_conf);
+	nop();
+#endif
 
-	while (true)
+	while (true) {
+		/* Read AGCCOUNTER */
+		spi_select_device(&SPI_AX, &g_ax_spi_device_conf);
+		g_ax_spi_packet_buffer[0]	= 0x43;
+		spi_write_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
+		memset(g_ax_spi_packet_buffer, 0, sizeof(g_ax_spi_packet_buffer));
+		spi_read_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
+		spi_deselect_device(&SPI_AX, &g_ax_spi_device_conf);
+
+		/* Read RSSI */
+		spi_select_device(&SPI_AX, &g_ax_spi_device_conf);
+		g_ax_spi_packet_buffer[0]	= 0x40;
+		spi_write_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
+		memset(g_ax_spi_packet_buffer, 0, sizeof(g_ax_spi_packet_buffer));
+		spi_read_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
+		spi_deselect_device(&SPI_AX, &g_ax_spi_device_conf);
+
+		/* Read TRK_AMPLITUDE */
+		spi_select_device(&SPI_AX, &g_ax_spi_device_conf);
+		g_ax_spi_packet_buffer[0]	= 0x48;
+		spi_write_packet(&SPI_AX, g_ax_spi_packet_buffer, 1);
+		memset(g_ax_spi_packet_buffer, 0, sizeof(g_ax_spi_packet_buffer));
+		spi_read_packet(&SPI_AX, g_ax_spi_packet_buffer, 2);
+		spi_deselect_device(&SPI_AX, &g_ax_spi_device_conf);
+
 		nop();
+	}
 }
