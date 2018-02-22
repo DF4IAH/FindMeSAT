36531670a04268ef4518f922465cfd8417a37990
Ulrich Habel <espero7757@gmx.net>
2018-02-22 21:42:16 +0100

    SW - PLL - time delay constant for initial setting re-timed

--- FindMeSAT_SW/FindMeSAT_SW/src/main.h
+++ FindMeSAT_SW/FindMeSAT_SW/src/main.h
@@ -48,7 +48,13 @@
 #define C_TCC1_MEAN_OFFSET											0
 #define C_TCC1_BORDER_OFFSET										3000
 #define C_TCC1_CLOCKSETTING_AFTER_SECS								30U
-#define C_TCC1_CLOCKSETTING_OFFSET									11U
+
+#ifdef DEBUG
+# define C_TCC1_CLOCKSETTING_OFFSET									27U
+#else
+# define C_TCC1_CLOCKSETTING_OFFSET									11U
+#endif
+
 #define C_TCC1_SPAN_HALF											50  // 30 equals to 1 µs
 
 
