76cbd5b23ea1aa0e2a96e1aafc7c83cdff9da732
Ulrich Habel <espero7757@gmx.net>
2018-04-01 21:17:32 +0200

    SW - interpreter - short hand for APRS (a) and POCSAG (p) now in use

--- FindMeSAT_SW/FindMeSAT_SW/src/main.h
+++ FindMeSAT_SW/FindMeSAT_SW/src/main.h
@@ -13,7 +13,7 @@
 
 
 /* VERSION: YYM, MDD */
-#define VERSION														20180330
+#define VERSION														20180401
 
 #define APPLICATION_NAME											"FindMeSAT"
 #define APPLICATION_VERSION											"1.0"
