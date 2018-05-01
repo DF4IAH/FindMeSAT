4e5b61e6179f5e7c7765d9d61596e28e81e5f9eb
Ulrich Habel <espero7757@gmx.net>
2018-05-01 23:12:26 +0200

    SW - V2 TrueSTUDIO - RTOS - GetMutexHolder activated

--- FindMeSAT_V2_SW/TrueSTUDIO/Inc/FreeRTOSConfig.h
+++ FindMeSAT_V2_SW/TrueSTUDIO/Inc/FreeRTOSConfig.h
@@ -143,6 +143,8 @@ to exclude the API function. */
 #define INCLUDE_xTaskGetSchedulerState      1
 #define INCLUDE_xEventGroupSetBitFromISR    1
 #define INCLUDE_xTimerPendFunctionCall      1
+#define INCLUDE_xQueueGetMutexHolder        1
+#define INCLUDE_xSemaphoreGetMutexHolder    1
 #define INCLUDE_pcTaskGetTaskName           1
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
 #define INCLUDE_xTaskGetCurrentTaskHandle   1
