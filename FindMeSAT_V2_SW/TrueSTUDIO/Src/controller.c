084f774d01be6bc1023e4149467a8e6fc63ecd38
Ulrich Habel <espero7757@gmx.net>
2018-05-04 23:32:03 +0200

    SW - V2 TrueSTUDIO - display of device package added

--- FindMeSAT_V2_SW/TrueSTUDIO/Src/controller.c
+++ FindMeSAT_V2_SW/TrueSTUDIO/Src/controller.c
@@ -111,10 +111,18 @@ void prvControllerInitAfterGreet(void)
   interpreterShowCursor();
 }
 
+const char controllerPackages0x00[] = "LQFP64";
+const char controllerPackages0x02[] = "LQFP100";
+const char controllerPackages0x03[] = "UFBGA132";
+const char controllerPackages0x04[] = "LQFP144, WLCSP81 or WLCSP72";
+const char controllerPackages0x10[] = "UFBGA169";
+const char controllerPackages0x11[] = "WLCSP100";
+const char controllerPackages0xXX[] = "(reserved)";
 void prvControllerPrintUID(void)
 {
   char lotBuf[8];
   char buf[80] = { 0 };
+  const char *packagePtr = NULL;
 
   uint32_t uidPosX    = (*((uint32_t*)  UID_BASE     )      ) & 0X0000ffffUL;
   uint32_t uidPosY    = (*((uint32_t*)  UID_BASE     ) >> 16) & 0X0000ffffUL;
@@ -123,7 +131,37 @@ void prvControllerPrintUID(void)
   memcpy(lotBuf, uidLot, 7);
   lotBuf[7] = 0;
 
-  sprintf(buf, "\tLot-Id:\t\t%s\r\n\tWafer:\t\t%lu\r\n\tPos. X/Y:\t%2lu/%2lu\r\n\r\n", lotBuf, uidWaf, uidPosX, uidPosY);
+  uint32_t package    = (*((uint32_t*)  PACKAGE_BASE))        & 0X0000001fUL;
+  switch(package) {
+  case 0x00:
+    packagePtr = controllerPackages0x00;
+    break;
+
+  case 0x02:
+    packagePtr = controllerPackages0x02;
+    break;
+
+  case 0x03:
+    packagePtr = controllerPackages0x03;
+    break;
+
+  case 0x04:
+    packagePtr = controllerPackages0x04;
+    break;
+
+  case 0x10:
+    packagePtr = controllerPackages0x10;
+    break;
+
+  case 0x11:
+    packagePtr = controllerPackages0x11;
+    break;
+
+  default:
+    packagePtr = controllerPackages0xXX;
+  }
+
+  sprintf(buf, "\tLot-Id:\t\t%s\r\n\tWafer:\t\t%lu\r\n\tPos. X/Y:\t%2lu/%2lu\r\n\tPackage(s):\t%s\r\n\r\n", lotBuf, uidWaf, uidPosX, uidPosY, packagePtr);
 
   osSemaphoreWait(usbToHostBinarySemHandle, 0);
   usbToHostWait((uint8_t*) buf, strlen(buf));
